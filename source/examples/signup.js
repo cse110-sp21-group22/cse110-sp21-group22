342eb4133c27d18737c71af1291bb3608554cade
RJ Wakefield-Carl <rjwc123@gmail.com>
2021-05-08 23:17:16 -0700

    Added user authentication example

---
+++ source/examples/signup.js
@@ -0,0 +1,28 @@
+const signupForm = document.getElementById("signup-form");
+signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = signupForm['email'].value;
+    const password = signupForm['password'].value;
+    // console.log(email, password);
+    signupForm.reset();
+    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+        // console.log(cred);
+        const userCredentials = {
+            email: email,
+            createdAt: new Date().toISOString(),
+            uid: cred.user.uid
+        };
+        return db.collection('users').doc(cred.user.uid).set(userCredentials).then(() => {
+            // console.log('success');
+            location = "login.html";
+        }).catch(err => {
+            console.log(err.message);
+            // const signupError = document.getElementById('signupError');
+            // signupError.innerText = err.message;
+        })
+    }).catch(err => {
+        console.log(err.message);
+        // const signupError2 = document.getElementById('signupError');
+        // signupError2.innerText = err.message;
+    })
+})
