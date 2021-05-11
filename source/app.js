01f343cfdab035b184a41ae2d70fec59ec0be728
RJ Wakefield-Carl <rjwc123@gmail.com>
2021-05-11 15:59:20 -0700

    Test jsdoc

--- source/app.js
+++ source/app.js
@@ -15,5 +15,5 @@ ColorPicker(
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
  */
- function Book(title, author) {
+function Book(title, author) {
 }
