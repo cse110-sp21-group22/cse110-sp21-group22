49947d4e745b2de3b6bbc95711d72b0cb5d7a70e
RJ Wakefield-Carl <rjwc123@gmail.com>
2021-05-11 02:21:21 -0700

    Add linter config

--- source/examples/app.js
+++ source/examples/app.js
@@ -2,71 +2,70 @@ const todoContainer = document.getElementById('todo-container');
 
 // checking if user is signed in or not
 auth.onAuthStateChanged(user => {
-    if (user) {
-        //console.log('user is signed in at index.html');
-    }
-    else {
-        //alert('your login session has expired or you have logged out, login again to continue');
-        //location = "login.html";
-        window.location.href = "login.html";
-    }
+  if (user) {
+    // console.log('user is signed in at index.html');
+  }
+  else {
+    // alert('your login session has expired or you have logged out, login again to continue');
+    // location = "login.html";
+    window.location.href = "login.html";
+  }
 })
 
 // retriving todos
 function renderData(individualDoc) {
+  // parent div
+  let parentDiv = document.createElement("div");
+  parentDiv.className = "container todo-box";
+  parentDiv.setAttribute('data-id', individualDoc.id);
 
-    // parent div
-    let parentDiv = document.createElement("div");
-    parentDiv.className = "container todo-box";
-    parentDiv.setAttribute('data-id', individualDoc.id);
+  // todo div
+  let todoDiv = document.createElement("div");
+  todoDiv.textContent = individualDoc.data().todos;
 
-    // todo div
-    let todoDiv = document.createElement("div");
-    todoDiv.textContent = individualDoc.data().todos;
+  // button
+  let trash = document.createElement("button");
 
-    // button
-    let trash = document.createElement("button");
+  let i = document.createElement("i");
+  i.className = "fas fa-trash";
 
-    let i = document.createElement("i");
-    i.className = "fas fa-trash";
+  // appending
+  trash.appendChild(i);
 
-    // appending
-    trash.appendChild(i);
+  parentDiv.appendChild(todoDiv);
+  parentDiv.appendChild(trash);
 
-    parentDiv.appendChild(todoDiv);
-    parentDiv.appendChild(trash);
+  // todoContainer.innerHTML += `
+  //     <div class="container todo-box" id ="${individualDoc.doc.id}">
+  //       <div>${individualDoc.doc.data().todos}</div>
+  //       <button onClick="deleteTodo('${individualDoc.doc.id}','${user.uid}')"><i class='fas fa-trash'></i></button>
+  //     </div>
+  //     `
+  todoContainer.appendChild(parentDiv);
 
-    // todoContainer.innerHTML += `
-    //     <div class="container todo-box" id ="${individualDoc.doc.id}">
-    //       <div>${individualDoc.doc.data().todos}</div>
-    //       <button onClick="deleteTodo('${individualDoc.doc.id}','${user.uid}')"><i class='fas fa-trash'></i></button>
-    //     </div>
-    //     `
-    todoContainer.appendChild(parentDiv);
-
-    // trash clicking event
-    trash.addEventListener('click', e => {
-        let id = e.target.parentElement.parentElement.getAttribute('data-id');
-        auth.onAuthStateChanged(user => {
-            if (user) {
-                fs.collection(user.uid).doc(id).delete();
-            }
-        })
+  // trash clicking event
+  trash.addEventListener('click', e => {
+    let id = e.target.parentElement.parentElement.getAttribute('data-id');
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        fs.collection(user.uid).doc(id).delete();
+      }
     })
+  })
 }
 
 // retriving username
 auth.onAuthStateChanged(user => {
-    const username = document.getElementById('username');
-    if (user) {
-        fs.collection('users').doc(user.uid).get().then((snapshot) => {
-            // console.log(snapshot.data().Name);
-            username.innerText = snapshot.data().name;
-        })
-    }
-    else {
-        // console.log('user is not signed in to retrive username');
-    }
+  const username = document.getElementById('username');
+  if (user) {
+    fs.collection('users').doc(user.uid).get().then((snapshot) => {
+      // console.log(snapshot.data().Name);
+      username.innerText = snapshot.data().name;
+    })
+  }
+  else {
+    // console.log('user is not signed in to retrive username');
+  }
 })
 
 // adding todos to firestore database
@@ -75,47 +74,47 @@ let date = new Date();
 let time = date.getTime();
 let counter = time;
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    const todos = form['todos'].value;
-    // console.log(todos);
-    let id = counter += 1;
-    form.reset();
-    auth.onAuthStateChanged(user => {
-        if (user) {
-            fs.collection(user.uid).doc('_' + id).set({
-                id: '_' + id,
-                todos
-            }).then(() => {
-                console.log('todo added');
-            }).catch(err => {
-                console.log(err.message);
-            })
-        }
-        else {
-            // console.log('user is not signed in to add todos');
-        }
-    })
+  e.preventDefault();
+  const todos = form['todos'].value;
+  // console.log(todos);
+  let id = counter += 1;
+  form.reset();
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      fs.collection(user.uid).doc('_' + id).set({
+        id: '_' + id,
+        todos
+      }).then(() => {
+        console.log('todo added');
+      }).catch(err => {
+        console.log(err.message);
+      })
+    }
+    else {
+      // console.log('user is not signed in to add todos');
+    }
+  })
 })
 
 // logout
 function logout() {
-    auth.signOut();
+  auth.signOut();
 }
 
 // realtime listners
 auth.onAuthStateChanged(user => {
-    if (user) {
-        fs.collection(user.uid).onSnapshot((snapshot) => {
-            let changes = snapshot.docChanges();
-            changes.forEach(change => {
-                if (change.type == "added") {
-                    renderData(change.doc);
-                }
-                else if (change.type == 'removed') {
-                    let li = todoContainer.querySelector('[data-id=' + change.doc.id + ']');
-                    todoContainer.removeChild(li);
-                }
-            })
-        })
-    }
+  if (user) {
+    fs.collection(user.uid).onSnapshot((snapshot) => {
+      let changes = snapshot.docChanges();
+      changes.forEach(change => {
+        if (change.type == "added") {
+          renderData(change.doc);
+        }
+        else if (change.type == 'removed') {
+          let li = todoContainer.querySelector('[data-id=' + change.doc.id + ']');
+          todoContainer.removeChild(li);
+        }
+      })
+    })
+  }
 })
--- source/examples/signup.js
+++ source/examples/signup.js
@@ -1,30 +1,30 @@
 const signupForm = document.getElementById("signup-form");
 signupForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const email = signupForm['email'].value;
-    const password = signupForm['password'].value;
-    const name = signupForm['name'].value;
-    // console.log(email, password);
-    signupForm.reset();
-    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-        // console.log(cred);
-        const userCredentials = {
-            name: name,
-            email: email,
-            createdAt: new Date().toISOString(),
-            uid: cred.user.uid
-        };
-        return db.collection('users').doc(cred.user.uid).set(userCredentials).then(() => {
-            // console.log('success');
-            location = "login.html";
-        }).catch(err => {
-            // console.log(err.message);
-            const signupError = document.getElementById('signupError');
-            signupError.innerText = err.message;
-        })
+  e.preventDefault();
+  const email = signupForm['email'].value;
+  const password = signupForm['password'].value;
+  const name = signupForm['name'].value;
+  // console.log(email, password);
+  signupForm.reset();
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    // console.log(cred);
+    const userCredentials = {
+      name: name,
+      email: email,
+      createdAt: new Date().toISOString(),
+      uid: cred.user.uid
+    };
+    return db.collection('users').doc(cred.user.uid).set(userCredentials).then(() => {
+      // console.log('success');
+      location = "login.html";
     }).catch(err => {
-        // console.log(err.message);
-        const signupError2 = document.getElementById('signupError');
-        signupError2.innerText = err.message;
+      // console.log(err.message);
+      const signupError = document.getElementById('signup-error');
+      signupError.innerText = err.message;
     })
+  }).catch(err => {
+    // console.log(err.message);
+    const signupError2 = document.getElementById('signup-error');
+    signupError2.innerText = err.message;
+  })
 })
