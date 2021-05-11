bd85f397c8fa5b35a8bcf2d826f5f636105e77cf
RJ Wakefield-Carl <rjwc123@gmail.com>
2021-05-11 02:05:10 -0700

    Added basic task list

--- source/examples/login.js
+++ source/examples/login.js
@@ -5,11 +5,11 @@ loginForm.addEventListener('submit', e => {
     const loginPassword = loginForm['password'].value;
     console.log(loginEmail, loginPassword);
     auth.signInWithEmailAndPassword(loginEmail, loginPassword).then(() => {
-        console.log('login success');
+        //console.log('login success');
         location = "index.html";
     }).catch(err => {
-        //const loginError = document.getElementById("loginError");
-        //loginError.innerText = err.message;
-        console.log(err);
+        const loginError = document.getElementById("loginError");
+        loginError.innerText = err.message;
+        //console.log(err);
     })
 })
--- source/examples/signup.js
+++ source/examples/signup.js
@@ -3,11 +3,13 @@ signupForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = signupForm['email'].value;
     const password = signupForm['password'].value;
+    const name = signupForm['name'].value;
     // console.log(email, password);
     signupForm.reset();
     auth.createUserWithEmailAndPassword(email, password).then((cred) => {
         // console.log(cred);
         const userCredentials = {
+            name: name,
             email: email,
             createdAt: new Date().toISOString(),
             uid: cred.user.uid
@@ -16,13 +18,13 @@ signupForm.addEventListener('submit', e => {
             // console.log('success');
             location = "login.html";
         }).catch(err => {
-            console.log(err.message);
-            // const signupError = document.getElementById('signupError');
-            // signupError.innerText = err.message;
+            // console.log(err.message);
+            const signupError = document.getElementById('signupError');
+            signupError.innerText = err.message;
         })
     }).catch(err => {
-        console.log(err.message);
-        // const signupError2 = document.getElementById('signupError');
-        // signupError2.innerText = err.message;
+        // console.log(err.message);
+        const signupError2 = document.getElementById('signupError');
+        signupError2.innerText = err.message;
     })
 })
