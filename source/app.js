ca4a1e39fedf86fe962237572cf5f81c1d298374
RJ Wakefield-Carl <rjwc123@gmail.com>
2021-05-11 15:56:33 -0700

    Test jsdoc

--- source/app.js
+++ source/app.js
@@ -8,3 +8,12 @@ ColorPicker(
   function(hex, hsv, rgb) {
     document.getElementById('navbar').style.backgroundColor = hex;
   });
+
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+ function Book(title, author) {
+}
