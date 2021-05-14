e44c8ce6d63c3e1a08a35cba96e616fa462f4bb5
Ajay Booter <abooter@ucsd.edu>
2021-05-13 21:55:13 -0700

    Implemented initial Quote Generator

--- source/app.js
+++ source/app.js
@@ -2,12 +2,13 @@
  * Get color from colorpicker and change navbar color.
  * @author Test
  * @example Test
- */
+ *
 ColorPicker(
   document.getElementById('color-picker'),
   function(hex, hsv, rgb) {
     document.getElementById('navbar').style.backgroundColor = hex;
   });
+*/
 
 /**
  * Represents a book.
@@ -17,3 +18,27 @@ ColorPicker(
  */
 function Book(title, author) {
 }
+
+/**
+ * Tasks that occur whenever the webpage is loaded
+ */
+ document.addEventListener('DOMContentLoaded', () => {
+
+        /* Quote Generator */
+
+        let url = "https://api.quotable.io/random";
+        let result;
+
+        //Fetches information from quote generator website
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+
+            //Updates html objects with content from the website
+            document.querySelector('#quote').innerHTML = "\"" + result.content + "\"";
+            document.querySelector('#authors').innerHTML = "-" + result.author;
+            });
+            
+        /*Quote Generator */ 
+
+}); /* document.addEventListener()*/
