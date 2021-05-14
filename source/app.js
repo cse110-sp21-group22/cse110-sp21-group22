2a2a53d823389a95876d38f3f1ab0f3d8139c94c
RJ Wakefield-Carl <rjwc1@users.noreply.github.com>
2021-05-13 22:05:14 -0700

    Update app.js

--- source/app.js
+++ source/app.js
@@ -2,13 +2,12 @@
  * Get color from colorpicker and change navbar color.
  * @author Test
  * @example Test
- *
+ */
 ColorPicker(
   document.getElementById('color-picker'),
   function(hex, hsv, rgb) {
     document.getElementById('navbar').style.backgroundColor = hex;
   });
-*/
 
 /**
  * Represents a book.
