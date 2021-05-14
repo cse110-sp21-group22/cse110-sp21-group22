9126c53aa510be81c32d0e2a122c77cd3c45e016
Restyled.io <commits@restyled.io>
2021-05-14 07:14:24 +0000

    Restyled by prettier

--- source/app.js
+++ source/app.js
@@ -1,6 +1,6 @@
 const PROGRESS_BAR = document.querySelector("#progressbar");
 const WEEK = document.getElementById("weeks");
-const {size} = PROGRESS_BAR.dataset;
+const { size } = PROGRESS_BAR.dataset;
 PROGRESS_BAR.style.width = `${0}%`;
 const today = new Date();
 const one_day_per_second = 1000 * 60 * 60 * 24;
@@ -26,28 +26,32 @@ function progress() {
     var date_diff = (semester_end - semester_start) / one_day_per_second;
     let weeks = (today - semester_start) / one_week_per_second;
     weeks = Math.round(weeks);
-    var text = "Welcome to Week " + weeks + "!" +
-               "😊";
+    var text = "Welcome to Week " + weeks + "!" + "😊";
     let progress = (today - semester_start) / one_day_per_second / date_diff;
     progress = Math.round(progress * 100);
-    if (progress > 100)
-      progress = 100;
-    if (progress <= 5)
-      PROGRESS_BAR.style.backgroundColor = "red";
+    if (progress > 100) progress = 100;
+    if (progress <= 5) PROGRESS_BAR.style.backgroundColor = "red";
     else if (progress <= 25 && progress > 5)
       PROGRESS_BAR.style.backgroundColor = "organge";
     else if (progress <= 50 && progress > 25)
       PROGRESS_BAR.style.backgroundColor = "gold";
     else if (progress <= 75 && progress > 50)
       PROGRESS_BAR.style.backgroundColor = "yellow";
-    else
-      PROGRESS_BAR.style.backgroundColor = "#86e01e";
+    else PROGRESS_BAR.style.backgroundColor = "#86e01e";
     PROGRESS_BAR.style.width = `${progress}%`;
     WEEK.innerHTML = text;
   }
 }
 
-$('#datepicker1').datepicker();
-$('#datepicker2').datepicker();
-$('#datepicker1').datepicker().on('changeDate', function(ev) { progress(); });
-$('#datepicker2').datepicker().on('changeDate', function(ev) { progress(); });
+$("#datepicker1").datepicker();
+$("#datepicker2").datepicker();
+$("#datepicker1")
+  .datepicker()
+  .on("changeDate", function (ev) {
+    progress();
+  });
+$("#datepicker2")
+  .datepicker()
+  .on("changeDate", function (ev) {
+    progress();
+  });
