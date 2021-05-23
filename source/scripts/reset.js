6746e58c2c42f688e7a2d784923f1c28404315d1
Restyled.io <commits@restyled.io>
2021-05-23 02:01:19 +0000

    Restyled by clang-format

--- source/scripts/reset.js
+++ source/scripts/reset.js
@@ -2,11 +2,10 @@ const loginForm = document.getElementById("reset-form");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const emailAddress = loginForm["email"].value;
-  auth
-    .sendPasswordResetEmail(emailAddress).then(function() {
-      location = "./login.html";
-    }).catch(function(err) {
-      const loginError = document.getElementById("reset-error");
-      loginError.innerText = err.message;  
-    });
+  auth.sendPasswordResetEmail(emailAddress)
+      .then(function() { location = "./login.html"; })
+      .catch(function(err) {
+        const loginError = document.getElementById("reset-error");
+        loginError.innerText = err.message;
+      });
 });
