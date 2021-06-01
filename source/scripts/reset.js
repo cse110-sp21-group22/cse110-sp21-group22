7341289cace6b810f2b1e00124206a1592a69b4a
Restyled.io <commits@restyled.io>
2021-06-01 05:48:01 +0000

    Restyled by clang-format

--- source/scripts/reset.js
+++ source/scripts/reset.js
@@ -2,18 +2,15 @@ const loginForm = document.getElementById("reset-form");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const emailAddress = loginForm["email"].value;
-  auth
-    .sendPasswordResetEmail(emailAddress)
-    .then(function () {
-      const loginError = document.getElementById("reset-error");
-      loginError.innerText =
-        "A password reset link has been sent to your email";
-      setTimeout(function () {
-        location = "./login.html";
-      }, 3000);
-    })
-    .catch(function (err) {
-      const loginError = document.getElementById("reset-error");
-      loginError.innerText = err.message;
-    });
+  auth.sendPasswordResetEmail(emailAddress)
+      .then(function() {
+        const loginError = document.getElementById("reset-error");
+        loginError.innerText =
+            "A password reset link has been sent to your email";
+        setTimeout(function() { location = "./login.html"; }, 3000);
+      })
+      .catch(function(err) {
+        const loginError = document.getElementById("reset-error");
+        loginError.innerText = err.message;
+      });
 });
