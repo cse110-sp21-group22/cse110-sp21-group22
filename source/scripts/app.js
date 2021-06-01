e3e9add2508f614d5f721ddb459655e0df1b253b
Restyled.io <commits@restyled.io>
2021-06-01 03:46:21 +0000

    Restyled by prettier

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -34,9 +34,14 @@ function NavbarLoaded() {
  * @returns day in year
  */
 function daysIntoYear(date) {
-  return ((Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-           Date.UTC(date.getFullYear(), 0, 0)) /
-          24 / 60 / 60 / 1000);
+  return (
+    (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+      Date.UTC(date.getFullYear(), 0, 0)) /
+    24 /
+    60 /
+    60 /
+    1000
+  );
 }
 
 /**
@@ -68,14 +73,16 @@ class BujoElement {
       if (user) {
         let dateYear = daysIntoYear(date) + selectedDate;
         fs.collection("users")
-            .doc(user.uid)
-            .collection("data")
-            .doc("notes")
-            .collection("" + dateYear)
-            .doc("" + this.id)
-            .withConverter(bujoConverter)
-            .set(this)
-            .catch((err) => { console.log(err.message); });
+          .doc(user.uid)
+          .collection("data")
+          .doc("notes")
+          .collection("" + dateYear)
+          .doc("" + this.id)
+          .withConverter(bujoConverter)
+          .set(this)
+          .catch((err) => {
+            console.log(err.message);
+          });
       }
     });
   }
@@ -88,13 +95,15 @@ class BujoElement {
       if (user) {
         let dateYear = daysIntoYear(date) + selectedDate;
         fs.collection("users")
-            .doc(user.uid)
-            .collection("data")
-            .doc("notes")
-            .collection("" + dateYear)
-            .doc("" + this.id)
-            .delete()
-            .catch((err) => { console.log(err.message); });
+          .doc(user.uid)
+          .collection("data")
+          .doc("notes")
+          .collection("" + dateYear)
+          .doc("" + this.id)
+          .delete()
+          .catch((err) => {
+            console.log(err.message);
+          });
       }
     });
   }
@@ -102,21 +111,27 @@ class BujoElement {
 
 // Firestore data converter
 var bujoConverter = {
-  toFirestore : function(bujo) {
+  toFirestore: function (bujo) {
     let dateYear = daysIntoYear(date) + selectedDate;
     return {
-      id : bujo.id,
-      text : bujo.text,
-      level : bujo.level,
-      type : bujo.type,
-      signifier : bujo.signifier,
-      style : bujo.style,
-      date : dateYear,
+      id: bujo.id,
+      text: bujo.text,
+      level: bujo.level,
+      type: bujo.type,
+      signifier: bujo.signifier,
+      style: bujo.style,
+      date: dateYear,
     };
   },
-  fromFirestore : function(snapshot, options) {
+  fromFirestore: function (snapshot, options) {
     const data = snapshot.data(options);
-    return new BujoElement(data.id, data.text, data.level, data.type,
-                           data.signifier, data.style);
+    return new BujoElement(
+      data.id,
+      data.text,
+      data.level,
+      data.type,
+      data.signifier,
+      data.style
+    );
   },
 };
