68e21efaf476d622b13ba1047c97b772fe2cbc34
Restyled.io <commits@restyled.io>
2021-06-10 00:01:14 +0000

    Restyled by clang-format

--- cypress/examples/waiting.spec.js
+++ cypress/examples/waiting.spec.js
@@ -1,9 +1,8 @@
 /// <reference types="cypress" />
 
 context("Waiting", () => {
-  beforeEach(() => {
-    cy.visit("https://example.cypress.io/commands/waiting");
-  });
+  beforeEach(
+      () => { cy.visit("https://example.cypress.io/commands/waiting"); });
   // BE CAREFUL of adding unnecessary wait times.
   // https://on.cypress.io/best-practices#Unnecessary-Waiting
 
@@ -26,8 +25,8 @@ context("Waiting", () => {
     cy.get(".network-btn").click();
 
     // wait for GET comments/1
-    cy.wait("@getComment")
-      .its("response.statusCode")
-      .should("be.oneOf", [200, 304]);
+    cy.wait("@getComment").its("response.statusCode").should("be.oneOf", [
+      200, 304
+    ]);
   });
 });
